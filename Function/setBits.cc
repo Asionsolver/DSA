4f5dc8d9a9e0c11b0b8e48c38aeaa741a99a5af5
asionsolver <asionsolver274@gmail.com>
2024-06-16 09:21:12 +0600

    solve new problem name like count all set bit of a number

--- Function/setBits.cc
+++ Function/setBits.cc
@@ -1,1 +1,22 @@
 // ! Count all set bit of a number
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout << "Enter any integer number: ";
+    cin >> n;
+
+    int k;
+    cout << "Enter kth bit: ";
+    cin >> k;
+
+    int mask = 1 << k;
+
+    int result = n | mask;
+    cout << "The final result is " << result << endl;
+
+    return 0;
+}
