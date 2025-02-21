6de8db8202bc2bb5c7ffa9dcfc81b3af22525a4c
asionsolver <asionsolver274@gmail.com>
2025-02-22 00:38:38 +0600

    learn array pointer deeply

---
+++ pointer/pointerArray.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+
+    // int arr[5] = {1,5,8,7,5};
+
+    // cout << arr << endl;
+    // cout << &arr << endl;
+    // cout<<&arr[0]<<endl;
+
+    // cout<<endl<<endl;
+    // cout<<"&arr[0] = "<<&arr[0]<<endl;
+    // cout<<"&arr[1] = "<<&arr[1]<<endl;
+    // cout<<"&arr[2] = "<<&arr[2]<<endl;
+    // cout<<"&arr[3] = "<<&arr[3]<<endl;
+    // cout<<"&arr[4] = "<<&arr[4]<<endl;
+
+    // cout<<endl<<endl;
+    // cout<<"arr = "<<arr<<endl;
+    // cout<<"arr + 1 = "<<arr + 1<<endl;
+    // cout<<"arr + 2 = "<<arr + 2<<endl;
+    // cout<<"arr + 3 = "<<arr + 3<<endl;
+    // cout<<"arr + 4 = "<<arr + 4<<endl;
+
+    // cout<<endl<<endl;
+    // arr = arr + 1; // error
+    // int arr2 = arr + 1; // error
+
+
+
+    // int *p = arr;
+    // int *q = arr + 1;
+    // int *r = arr + 2;
+    // int *s = arr + 3;
+    // int *t = arr + 4;
+
+
+    // cout<<endl<<endl;
+    // cout<<"p = arr: "<< p<<endl;
+    // cout<<"q = arr + 1: "<< q<<endl;
+    // cout<<"r = arr + 2: "<< r<<endl;
+    // cout<<"s = arr + 3: "<< s<<endl;
+    // cout<<"t = arr + 4: "<< t<<endl;
+
+    // cout<<endl<<endl;
+    // cout<<"*p = arr: "<< *p<<endl;
+    // cout<<"*q = arr + 1: "<< *q<<endl;
+    // cout<<"*r = arr + 2: "<< *r<<endl;
+    // cout<<"*s = arr + 3: "<< *s<<endl;
+    // cout<<"*t = arr + 4: "<< *t<<endl;
+
+
+    // cout<<*p + 3<<endl;
+    // cout<<*(arr+1)<<endl;
+    // cout<<1[arr]<<endl;
+    // cout<<&p<<endl;
+
+    // cout<<endl<<endl;
+    // cout<< "size of arr: "<<sizeof(arr)<<endl;
+    // cout<<"size of p: "<<sizeof(p)<<endl;
+    // cout<<"size of *p: "<<sizeof(*p)<<endl; 
+
+
+    // char charArr[5] = {'a','b','c','d','e'};
+    // char *charP = charArr;
+
+    // cout<<endl<<endl;
+    // cout<<"charArr: "<<charArr<<endl;
+    // cout<<"&charArr: "<<&charArr<<endl;
+    // cout<<"charP: "<<charP<<endl;
+    // cout<<"*charP: "<<*charP<<endl;
+    // cout<<"&charP: "<<&charP<<endl;
+
+    // char str[] = "HelloHello";
+    // char *p = str;
+
+    // cout<<endl<<endl;
+    // cout<<"p = "<<p<<endl;
+    // cout<<"p + 1 = "<<p + 1<<endl;
+    // cout<<"p + 2 = "<<p + 2<<endl;
+    // cout<<"p + 3 = "<<p + 3<<endl;
+    // cout<<"p + 4 = "<<p + 4<<endl;
+    // cout<<"p + 5 = "<<p + 5<<endl;
+
+    char ch = 'a';
+    char *p = &ch;
+    cout<<p<<endl;
+    cout<<*p<<endl;
+    cout<<&p<<endl;
+
+
+    return 0;
+}
--- pointer/tempCodeRunnerFile.cc
+++
@@ -1,5 +0,0 @@
- "<<&a<<endl;
-    // cout<<"Value of a: "<<a<<endl;
-    // cout<<"Address of ptr: "<<ptr<<endl;
-    // cout<<"Value of ptr: "<<*ptr<<endl;
-    // cout<<"Address of ptr: "<<&ptr<<endl;
