30bc1e5f233a6e949ee51e566605a2ef3978b81c
asionsolver <asionsolver274@gmail.com>
2025-08-26 08:23:30 +0600

    solve dutch national flag algorithm  related problem

--- problem/array/moveNegative.cc
+++ problem/array/moveNegative.cc
@@ -1,3 +1,5 @@
+// ! Question: Move all -ve number to left side of an array
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -5,7 +7,46 @@ using namespace std;
 
 int main()
 {
-    vector<int> arr{1, 2, -3, 4, -5, 6};
+    vector<int> arr{-1, 2, -3, 4, -5, 6};
+
+    int low = 0;
+    int high = arr.size() - 1;
+
+    while (low < high)
+    {
+        if (arr[low] > 0 && arr[high] > 0)
+        {
+            high--;
+        }
+        else if (arr[low] > 0 && arr[high] < 0)
+        {
+            // cout << "before" << endl;
+            // cout << arr[low] << endl;
+            // cout << arr[high] << endl;
+            swap(arr[low], arr[high]);
+            // cout << "after" << endl;
+            // cout << arr[low] << endl;
+            // cout << arr[high] << endl;
+            low++;
+            high--;
+        }
+        else if (arr[low] < 0 && arr[high] > 0)
+        {
+            low++;
+            high--;
+        }
+        else
+        {
+            low++;
+        }
+    }
+
+    for (int v : arr)
+        cout << v << " ";
+    cout << endl;
 
     return 0;
 }
+
+// T.C: O(n)
+// S.C: O(1)
