8596bb3f2e75d2dc54b4591f5ed1457b1223e593
asionsolver <asionsolver274@gmail.com>
2025-08-31 17:13:19 +0600

    solve leetcode problem: 35. Search Insert Position

---
+++ LeetCode/searching/searchInsertPosition.cc
@@ -0,0 +1,47 @@
+// 35. Search Insert Position
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int searchInsertPosition(vector<int> nums, int target)
+{
+    int start = 0;
+    int end = nums.size() - 1;
+    int mid = start + (end - start) / 2;
+    bool flag = false;
+    while (start <= end)
+    {
+        if (nums[mid] == target)
+        {
+            flag = true;
+            return mid;
+            break;
+        }
+        else if (nums[mid] > target)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+
+        mid = start + (end - start) / 2;
+    }
+    if (flag == false)
+    {
+
+        return start;
+    }
+
+    return -1;
+}
+int main()
+{
+    vector<int> nums = {1, 3, 4, 5, 9, 15, 18, 21, 24};
+    int target = 2;
+    int result = searchInsertPosition(nums, target);
+    cout << "Search Insert Position: " << result << endl;
+    return 0;
+}
