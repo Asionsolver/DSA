2ff26aed1bbdfee2647114d9696e54bf34ee1d22
asionsolver <asionsolver274@gmail.com>
2025-09-07 21:06:17 +0600

    add new example in pointerArray.cpp file

--- pointer/basic.cc
+++ pointer/basic.cc
@@ -6,148 +6,148 @@ using namespace std;
 int main()
 {
 
-    // int a = 5;
-    // int b = 5;
-    // int c = 5;
-    // int d = 5;
+     // int a = 5;
+     // int b = 5;
+     // int c = 5;
+     // int d = 5;
 
-    // cout<<"Address of a: "<<&a<<endl;
-    // cout<<"Address of b: "<<&b<<endl;
-    // cout<<"Address of c: "<<&c<<endl;
-    // cout<<"Address of d: "<<&d<<endl;
+     // cout<<"Address of a: "<<&a<<endl;
+     // cout<<"Address of b: "<<&b<<endl;
+     // cout<<"Address of c: "<<&c<<endl;
+     // cout<<"Address of d: "<<&d<<endl;
 
-    // int a = 5;
+     // int a = 5;
 
-    // pointer create
-    // int* ptr = &a;
+     // pointer create
+     // int* ptr = &a;
 
-    // cout<<"Address of a: "<<&a<<endl;
-    // cout<<"Value of a: "<<a<<endl;
+     // cout<<"Address of a: "<<&a<<endl;
+     // cout<<"Value of a: "<<a<<endl;
 
-    // access the address of a
-    // cout<<"Address of ptr: "<<ptr<<endl;
+     // access the address of a
+     // cout<<"Address of ptr: "<<ptr<<endl;
 
-    // access the value ptr is pointing to
-    // cout<<"Value of ptr: "<<*ptr<<endl;
+     // access the value ptr is pointing to
+     // cout<<"Value of ptr: "<<*ptr<<endl;
 
-    // address of ptr
-    // cout<<"Address of ptr: "<<&ptr<<endl;
+     // address of ptr
+     // cout<<"Address of ptr: "<<&ptr<<endl;
 
-    // int a = 5;
+     // int a = 5;
 
-    // int* ptr = &a; // pointer create
+     // int* ptr = &a; // pointer create
 
-    // char ch = 'A';
+     // char ch = 'A';
 
-    // char* ptr1 = &ch; // pointer create
+     // char* ptr1 = &ch; // pointer create
 
-    // double d = 5.5;
+     // double d = 5.5;
 
-    // double* ptr2 = &d; // pointer create
+     // double* ptr2 = &d; // pointer create
 
-    // size of pointer
-    // cout<<"Size of int pointer: "<<sizeof(ptr)<<endl; // Size of int pointer: 4
-    // cout<<"Size of char pointer: "<<sizeof(ptr1)<<endl; // Size of char pointer: 4
-    // cout<<"Size of double pointer: "<<sizeof(ptr2)<<endl; // Size of double pointer: 4
+     // size of pointer
+     // cout<<"Size of int pointer: "<<sizeof(ptr)<<endl; // Size of int pointer: 4
+     // cout<<"Size of char pointer: "<<sizeof(ptr1)<<endl; // Size of char pointer: 4
+     // cout<<"Size of double pointer: "<<sizeof(ptr2)<<endl; // Size of double pointer: 4
 
-    // int* ptr; // Bad practice: pointer is not initialized
-    // cout<<"Value of ptr: "<<*ptr<<endl; // segmentation fault
+     // int* ptr; // ! Bad practice: pointer is not initialized
+     // cout<<"Value of ptr: "<<*ptr<<endl; // segmentation fault
 
-    // int *ptr = 0; // Good practice: pointer is initialized with 0
-    // cout<<"Value of ptr: "<<*ptr<<endl; // segmentation fault
+     // int *ptr = 0; // Good practice: pointer is initialized with 0
+     // cout<<"Value of ptr: "<<*ptr<<endl; // segmentation fault
 
-    // int* ptr1 = NULL; // Good practice: pointer is initialized with NULL
-    // cout<<"Value of ptr1: "<<*ptr1<<endl;
+     // int* ptr1 = NULL; // Good practice: pointer is initialized with NULL
+     // cout<<"Value of ptr1: "<<*ptr1<<endl;
 
-    // int* ptr = nullptr; // Good practice: pointer is initialized with nullptr
-    // cout<<"Value of ptr: "<<*ptr<<endl;
+     // int* ptr = nullptr; // Good practice: pointer is initialized with nullptr
+     // cout<<"Value of ptr: "<<*ptr<<endl;
 
-    // int a = 5;
-    // int* ptr = &a;
+     // int a = 5;
+     // int* ptr = &a;
 
-    // cout<<"Address of a: "<<&a<<endl;
-    // cout<<"Value of a: "<<a<<endl;
-    // cout<<"Address of ptr: "<<ptr<<endl;
-    // cout<<"Value of ptr: "<<*ptr<<endl;
+     // cout<<"Address of a: "<<&a<<endl;
+     // cout<<"Value of a: "<<a<<endl;
+     // cout<<"Address of ptr: "<<ptr<<endl;
+     // cout<<"Value of ptr: "<<*ptr<<endl;
 
-    // cout<<endl<<endl<<endl;
-    // a = a + 1;
+     // cout<<endl<<endl<<endl;
+     // a = a + 1;
 
-    // cout<<"Address of a: "<<&a<<endl;
-    // cout<<"Value of a: "<<a<<endl;
-    // cout<<"Address of ptr: "<<ptr<<endl;
-    // cout<<"Value of ptr: "<<*ptr<<endl;
+     // cout<<"Address of a: "<<&a<<endl;
+     // cout<<"Value of a: "<<a<<endl;
+     // cout<<"Address of ptr: "<<ptr<<endl;
+     // cout<<"Value of ptr: "<<*ptr<<endl;
 
-    // cout<<endl<<endl<<endl;
-    // ptr = ptr + 1;
+     // cout<<endl<<endl<<endl;
+     // ptr = ptr + 1;
 
-    // cout<<"Address of ptr: "<<ptr<<endl;
+     // cout<<"Address of ptr: "<<ptr<<endl;
 
-    // cout<<endl<<endl<<endl;
-    // *ptr = *ptr + 1;
+     // cout<<endl<<endl<<endl;
+     // *ptr = *ptr + 1;
 
-    // cout<<"Value of *ptr: "<<*ptr<<endl;
+     // cout<<"Value of *ptr: "<<*ptr<<endl;
 
-    int a = 5;
+     int a = 5;
 
-    int *ptr = &a;
+     int *ptr = &a;
 
-    // copy of pointer
-    int *copyPtr = ptr;
+     // copy of pointer
+     int *copyPtr = ptr;
 
-    cout << "Address of a: " << &a << endl;
-    cout << "Value of a: " << a << endl;
+     cout << "Address of a: " << &a << endl;
+     cout << "Value of a: " << a << endl;
 
-    cout << endl
-         << endl
-         << endl;
-    cout << "ptr" << endl;
-    cout << "Address of ptr: " << ptr << endl;
-    cout << "Value of ptr: " << *ptr << endl;
-    cout << "Address of ptr: " << &ptr << endl;
+     cout << endl
+          << endl
+          << endl;
+     cout << "ptr" << endl;
+     cout << "Address of ptr: " << ptr << endl;
+     cout << "Value of ptr: " << *ptr << endl;
+     cout << "Address of ptr: " << &ptr << endl;
 
-    cout << endl
-         << endl
-         << endl;
-    cout << "copyPtr" << endl;
-    cout << "Address of copyPtr: " << copyPtr << endl;
-    cout << "Value of copyPtr: " << *copyPtr << endl;
-    cout << "Address of copyPtr: " << &copyPtr << endl;
+     cout << endl
+          << endl
+          << endl;
+     cout << "copyPtr" << endl;
+     cout << "Address of copyPtr: " << copyPtr << endl;
+     cout << "Value of copyPtr: " << *copyPtr << endl;
+     cout << "Address of copyPtr: " << &copyPtr << endl;
 
-    // cout<< "*, +, -, /, %, ++"<<endl;
-    // cout<<"Value of *ptr * 2: "<<*ptr * 2<<endl;
-    // cout<<"Value of *ptr + 2: "<<*ptr + 2<<endl;
-    // cout<<"Value of *ptr - 2: "<<*ptr - 2<<endl;
-    // cout<<"Value of *ptr / 2: "<<*ptr / 2<<endl;
-    // cout<<"Value of *ptr % 2: "<<*ptr % 2<<endl;
+     // cout<< "*, +, -, /, %, ++"<<endl;
+     // cout<<"Value of *ptr * 2: "<<*ptr * 2<<endl;
+     // cout<<"Value of *ptr + 2: "<<*ptr + 2<<endl;
+     // cout<<"Value of *ptr - 2: "<<*ptr - 2<<endl;
+     // cout<<"Value of *ptr / 2: "<<*ptr / 2<<endl;
+     // cout<<"Value of *ptr % 2: "<<*ptr % 2<<endl;
 
-    // cout<<endl<<endl<<endl;
-    // cout<< "Post increment operator: ++"<<endl;
-    // cout<<"Value of *ptr: "<<*ptr<<endl;
-    // cout<<"Value of *ptr++: "<<*ptr++<<endl;
-    // cout<<"Value of *ptr: "<<*ptr<<endl;
-    // cout<< "Post increment operator: ++"<<endl;
+     // cout<<endl<<endl<<endl;
+     // cout<< "Post increment operator: ++"<<endl;
+     // cout<<"Value of *ptr: "<<*ptr<<endl;
+     // cout<<"Value of *ptr++: "<<*ptr++<<endl;
+     // cout<<"Value of *ptr: "<<*ptr<<endl;
+     // cout<< "Post increment operator: ++"<<endl;
 
-    // cout<<endl<<endl<<endl;
-    // cout<< "Pre increment operator: ++"<<endl;
-    // cout<<"Value of *ptr: "<<*ptr<<endl;
-    // cout<<"Value of ++*ptr: "<<++*ptr<<endl;
-    // cout<<"Value of *ptr: "<<*ptr<<endl;
-    // cout<< "Pre increment operator: ++"<<endl;
+     // cout<<endl<<endl<<endl;
+     // cout<< "Pre increment operator: ++"<<endl;
+     // cout<<"Value of *ptr: "<<*ptr<<endl;
+     // cout<<"Value of ++*ptr: "<<++*ptr<<endl;
+     // cout<<"Value of *ptr: "<<*ptr<<endl;
+     // cout<< "Pre increment operator: ++"<<endl;
 
-    // cout<<endl<<endl<<endl;
-    // cout<< "Post decrement operator: --"<<endl;
-    // cout<<"Value of *ptr: "<<*ptr<<endl;
-    // cout<<"Value of *ptr--: "<<*ptr--<<endl;
-    // cout<<"Value of *ptr: "<<*ptr<<endl;
-    // cout<< "Post decrement operator: --"<<endl;
+     // cout<<endl<<endl<<endl;
+     // cout<< "Post decrement operator: --"<<endl;
+     // cout<<"Value of *ptr: "<<*ptr<<endl;
+     // cout<<"Value of *ptr--: "<<*ptr--<<endl;
+     // cout<<"Value of *ptr: "<<*ptr<<endl;
+     // cout<< "Post decrement operator: --"<<endl;
 
-    // cout<<endl<<endl<<endl;
-    // cout<< "Pre decrement operator: --"<<endl;
-    // cout<<"Value of *ptr: "<<*ptr<<endl;
-    // cout<<"Value of --*ptr: "<<--*ptr<<endl;
-    // cout<<"Value of *ptr: "<<*ptr<<endl;
-    // cout<< "Pre decrement operator: --"<<endl;
+     // cout<<endl<<endl<<endl;
+     // cout<< "Pre decrement operator: --"<<endl;
+     // cout<<"Value of *ptr: "<<*ptr<<endl;
+     // cout<<"Value of --*ptr: "<<--*ptr<<endl;
+     // cout<<"Value of *ptr: "<<*ptr<<endl;
+     // cout<< "Pre decrement operator: --"<<endl;
 
-    return 0;
+     return 0;
 }
--- pointer/pointerArray.cpp
+++ pointer/pointerArray.cpp
@@ -8,9 +8,9 @@ int main()
 
     // int arr[5] = {1,5,8,7,5};
 
-    // cout << arr << endl;
-    // cout << &arr << endl;
-    // cout<<&arr[0]<<endl;
+    // cout << arr << endl; // base address
+    // cout << &arr << endl; // base address
+    // cout<<&arr[0]<<endl; // 0th location address
 
     // cout<<endl<<endl;
     // cout<<"&arr[0] = "<<&arr[0]<<endl;
@@ -80,6 +80,27 @@ int main()
     // cout<<"p + 3 = "<<p + 3<<endl;
     // cout<<"p + 4 = "<<p + 4<<endl;
     // cout<<"p + 5 = "<<p + 5<<endl;
+
+    int arr[4] = {5, 6, 7, 8};
+    int *p = arr;
+    int *q = arr + 1;
+
+    cout << "arr-> " << arr << endl;         // base address
+    cout << "&arr-> " << &arr << endl;       // base address
+    cout << "arr[0]-> " << arr[0] << endl;   // 5
+    cout << "&arr[0]-> " << &arr[0] << endl; // base address
+
+    cout << "p-> " << p << endl;           // base addres
+    cout << "&p-> " << &p << endl;         // p address
+    cout << "*p-> " << *p << endl;         // 5
+    cout << "*p + 1-> " << *p + 1 << endl; // 6
+    cout << "*p + 2-> " << *p + 2 << endl; // 7
+
+    cout << "q-> " << p << endl;               // arr 1th position address
+    cout << "&q-> " << &q << endl;             // q address
+    cout << "*q-> " << *q << endl;             // 6
+    cout << "*q + 3-> " << *q + 3 << endl;     // 9
+    cout << "*(q + 4)-> " << *(q + 4) << endl; // out of bound index or garbage value
 
     return 0;
 }
