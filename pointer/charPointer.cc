58ccc9fd8b52f6eeaafeed27fe327d1912c79a38
asionsolver <asionsolver274@gmail.com>
2025-09-08 09:18:55 +0600

    deep dive pointer char array  and practice multiple char pointer related problems

--- pointer/charPointer.cc
+++ pointer/charPointer.cc
@@ -6,16 +6,33 @@ using namespace std;
 int main()
 {
 
-    char ch = 'a';
-    char *p = &ch;
-    cout << p << endl;
-    cout << *p << endl;
-    cout << &p << endl;
+    char ch[10] = "Ashis";
+    char *p = ch;
 
-    // char name[10] = "Hello";
-    // cout << "Name: " << name << endl;
-    // char *q = "Hello"; // ! Bad practice
-    // cout << "q: " << q << endl;
+    cout << "ch-> " << ch << endl;       // Ashis
+    cout << "ch+2-> " << ch + 2 << endl; // his
+    cout << "&ch-> " << &ch << endl;     // Base Address
+    cout << "ch[0]-> " << ch[0] << endl; // A
+
+    cout << "p-> " << p << endl;            // his
+    cout << "p+2-> " << p + 2 << endl;      // Ashis
+    cout << "&p-> " << &p << endl;          // p address
+    cout << "*p-> " << *p << endl;          // A <- *(p + 0)
+    cout << "*(p+3)->" << *(p + 3) << endl; // i <- *(p + 3)
+
+    char s = 'k';
+    char *cp = &s;
+
+    cout << "s-> " << s << endl; // k
+
+    cout << "cp-> " << cp << endl;   // k����
+    cout << "&cp-> " << &cp << endl; // cp address
+    cout << "*cp-> " << *cp << endl; // k <- *(cp + 0)
+
+    // char name[10] = "Hello"; // ? Good practice because its copy array storage
+    // cout << "Name: " << name << endl; // Hello
+    // char *q = "Hello"; // ! Bad practice because it's point temporary storage
+    // cout << "q: " << q << endl; // Hello
 
     return 0;
 }
