082d1b5ce74da82586e2d28542df1a98f65b7226
asionsolver <asionsolver274@gmail.com>
2025-09-15 20:22:14 +0600

    add new way to solve array max element using recursion

--- recursion/arrayMax.cc
+++ recursion/arrayMax.cc
@@ -6,31 +6,41 @@ using namespace std;
 
 // ! print maximum number in array element using recursion
 
-void maxElement(int arr[], int n, int &max)
+// void maxElement(int arr[], int n, int &max)
+// {
+
+//     // base case
+//     if (n == 0)
+//     {
+//         return;
+//     }
+
+//     // processing
+//     if (max < arr[n - 1])
+//     {
+//         max = arr[n - 1];
+//     }
+
+//     // Then call nth recursive case
+//     maxElement(arr, n - 1, max);
+// }
+
+int maxElement(int arr[], int n, int idx)
 {
-
-    // base case
-    if (n == 0)
-    {
-        return;
-    }
-
-    // processing
-    if (max < arr[n - 1])
-    {
-        max = arr[n - 1];
-    }
-
-    // Then call nth recursive case
-    maxElement(arr, n - 1, max);
+    if (idx == n)
+        return INT_MIN;
+    return max(arr[idx], maxElement(arr, n, idx + 1));
 }
 
 int main()
 {
     int arr[] = {10, 90, 70, 40, 50};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int max = INT_MIN;
-    maxElement(arr, n, max);
+    // int max = INT_MIN;
+    // maxElement(arr, n, max);
+    // cout << "Maximum number is " << max << endl;
+
+    int max = maxElement(arr, n, 0);
 
     cout << "Maximum number is " << max << endl;
     return 0;
